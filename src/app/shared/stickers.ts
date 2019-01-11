bbbb5d9e9c88b02b5a2e5a852fcdb2e5768309ea
Roeland Verbakel <roeland@handihow.com>
2019-01-11 19:00:15 +0100

    modify some stickers

--- src/app/shared/stickers.ts
+++ src/app/shared/stickers.ts
@@ -9,12 +9,12 @@ export class Stickers {
 		    {id: 4, asset: "/assets/stickers/wow.png", description: "Wow!"},
 		    {id: 5, asset: "/assets/stickers/top.png", description: "Top!"},
 		    {id: 6, asset: "/assets/stickers/yes.png", description: "Yes!"},
-		    {id: 7, asset: "/assets/stickers/een_wit.png", description: "Nummer 1, wit"},
-		    {id: 8, asset: "/assets/stickers/ster_goud.png", description: "Ster, goud"},
-		    {id: 9, asset: "/assets/stickers/ster_zilver.png", description: "Ster, zilver"},
-		    {id: 10, asset: "/assets/stickers/ster_brons.png", description: "Ster, brons"},
-		    {id: 11, asset: "/assets/stickers/bloem_blauw.png", description: "Bloem, blauw"},
-		    {id: 12, asset: "/assets/stickers/bloem_geel.png", description: "Bloem, geel"}
+		    {id: 7, asset: "/assets/stickers/ster_goud.png", description: "Ster, goud"},
+		    {id: 8, asset: "/assets/stickers/ster_zilver.png", description: "Ster, zilver"},
+		    {id: 9, asset: "/assets/stickers/ster_brons.png", description: "Ster, brons"},
+		    {id: 10, asset: "/assets/stickers/bloem_blauw.png", description: "Bloem, blauw"},
+		    {id: 11, asset: "/assets/stickers/bloem_geel.png", description: "Bloem, geel"},
+		    {id: 12, asset: "/assets/stickers/bloem_groen.png", description: "Bloem, groen"}
 		  ];
 	
 }
